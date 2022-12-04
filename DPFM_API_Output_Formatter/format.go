e2f52b1ce6e2fd8e29eb9c880915595b642dc290
Yurino Oga <yurino.o@latona.io>
2022-12-04 10:19:38 +0900

    release

--- DPFM_API_Output_Formatter/format.go
+++ DPFM_API_Output_Formatter/format.go
@@ -2,8 +2,6 @@ package dpfm_api_output_formatter
 
 import (
 	dpfm_api_input_reader "convert-to-dpfm-payment-requisition-mitsubishi-from-dpfm/DPFM_API_Input_Reader"
-	"fmt"
-	"strconv"
 )
 
 func ConvertToHeader(sdc *dpfm_api_input_reader.SDC) (*Header, error) {
@@ -53,18 +51,3 @@ func ConvertToItem(sdc *dpfm_api_input_reader.SDC) (*Item, error) {
 	return &item, nil
 }
 */
-
-func parseFolat32Ptr(s string) (*float32, error) {
-	resFloat64, err := strconv.ParseFloat(s, 32)
-	if err != nil {
-		fmt.Printf("%s\n", err.Error())
-		return nil, err
-	}
-	resFloat32 := getFloat32Ptr(float32(resFloat64))
-
-	return resFloat32, nil
-}
-
-func getFloat32Ptr(f float32) *float32 {
-	return &f
-}
